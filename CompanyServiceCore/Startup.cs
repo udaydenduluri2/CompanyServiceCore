8028621be29808baddb19bc340972a34a93e3a47
udaydenduluri2 <40038429+udaydenduluri2@users.noreply.github.com>
2018-07-05 15:01:34 +0100

    hh

--- CompanyServiceCore/Startup.cs
+++ CompanyServiceCore/Startup.cs
@@ -42,7 +42,7 @@ namespace CompanyServiceCore
             //{
             //    options.Filters.Add(new CorsAuthorizationFilterFactory("MyPolicy"));
             //});
-            services.AddMvc();
+            services.AddMvcCore().AddJsonFormatters();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
