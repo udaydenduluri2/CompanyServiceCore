62a649318b9869010a1229b123b4202f4f665f1c
udaydenduluri2 <40038429+udaydenduluri2@users.noreply.github.com>
2018-07-05 14:03:18 +0100

    grg

--- CompanyServiceCore/Startup.cs
+++ CompanyServiceCore/Startup.cs
@@ -28,7 +28,8 @@ namespace CompanyServiceCore
             {
                 builder.AllowAnyOrigin()
                        .AllowAnyMethod()
-                       .AllowAnyHeader();
+                       .AllowAnyHeader()
+                       .AllowCredentials();
             }));
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
